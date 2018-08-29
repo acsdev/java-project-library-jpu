d26eb746c6414444ce43045c4b1657ed1874a222
acsdev <allancarneirosantos@gmail.com>
2018-08-29 11:47:06 -0300

    testWithSupplier

--- eti.jpu/src/test/java/eti/jpu/CaseTest.java
+++ eti.jpu/src/test/java/eti/jpu/CaseTest.java
@@ -1,6 +1,7 @@
 package eti.jpu;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 import org.junit.Assert;
 
@@ -12,7 +13,6 @@ public class CaseTest {
     
     @Test
     public void testWithRunnable() {
-        
         Runnable rPrintTrue  = () -> System.out.println( "TRUE" );
         Runnable rPrintFalse = () -> System.out.println( "FALSE" );
 
@@ -28,5 +28,24 @@ public class CaseTest {
 
         Case.of( Optional.empty() ).trueRun( rPrintTrue ).falseRun( rPrintFalse );
     }
-    
+        
+    @Test
+    public void testWithSupplier() {
+
+        Supplier<String> sPrintTrue  = () -> "TRUE";
+        Supplier<String> sPrintFalse = () -> "FALSE";
+
+        System.out.println( Case.of( "A".equalsIgnoreCase("a") ).trueGet( sPrintTrue ).falseGet( sPrintFalse ).getResult().get() );
+        
+        System.out.println( Case.of( ( s ) -> "A".equalsIgnoreCase(s), "A" ).trueGet( sPrintTrue ).falseGet( sPrintFalse ).getResult().get() );
+
+        System.out.println( Case.of( Optional.of("A") ).trueGet( sPrintTrue ).falseGet( sPrintFalse ).getResult().get() );
+
+        System.out.println( Case.of( "A".equalsIgnoreCase("b") ).trueGet( sPrintTrue ).falseGet( sPrintFalse ).getResult().get() );
+        
+        System.out.println( Case.of( ( s ) -> "A".equalsIgnoreCase(s), "b" ).trueGet( sPrintTrue ).falseGet( sPrintFalse ).getResult().get() );
+
+        System.out.println( Case.of( Optional.empty() ).trueGet( sPrintTrue ).falseGet( sPrintFalse ).getResult().get() );
+
+    }
 }
